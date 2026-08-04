92a79080a2777dca2ed2f3fd891b7e39bf2b6b26
Luca <70321124+lucamergell@users.noreply.github.com>
2026-08-04 20:33:11 +0400

    vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Analytics } from '@vercel/analytics/react';
 import { AppProvider, useApp } from './context/AppContext';
 import { LanguageProvider, useLanguage } from './context/LanguageContext';
 import { Navbar } from './components/Navbar';
@@ -106,6 +107,7 @@ export default function App() {
     <LanguageProvider>
       <AppProvider>
         <MainContent />
+        <Analytics />
       </AppProvider>
     </LanguageProvider>
   );
